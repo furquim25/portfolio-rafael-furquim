3a1e749e6b63f20d8df19b7b92cbea007a56eb79
Rafael Furquim <rafa_furquim@nidf.ufrj.br>
2022-08-28 17:46:01 -0300

    sections about and skills added

--- static/script.js
+++ static/script.js
@@ -8,3 +8,18 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
 })
+
+/* Closes the menu when clickin an item on the menu*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
+
+console.log(navItem)
