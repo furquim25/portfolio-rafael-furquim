48943365877196c9f98e92c75945c5f71aee5544
Rafael Furquim <rafa_furquim@nidf.ufrj.br>
2022-09-07 20:05:27 -0300

    Some data corrections and footer added

--- static/script.js
+++ static/script.js
@@ -22,4 +22,23 @@ navItem.forEach(item => {
     })
 })
 
-console.log(navItem)
+// Animate all items with data-anime atributte
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    item.forEach(element => {
+        if (windowTop > element.offsetTop){
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate")
+        }
+    });
+};
+
+animeScroll()
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
